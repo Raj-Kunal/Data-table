abc9e28aab53633dc43404705d5aadbed1709ca7
Kunal Raj <imkunalraj15@gmail.com>
2022-07-16 16:34:56 +0530

    slice error

--- pages/table.js
+++ pages/table.js
@@ -4,8 +4,8 @@ import styles from '../styles/Home.module.css'
 
 const Table = ({ dataKeys, data, query }) => {
 
-  const [start, setStart] = useState(0)
-  const [count, setCount] = useState(10)
+  // const [start, setStart] = useState(0)
+  // const [count, setCount] = useState(10)
   const [pageNumber, setPageNumber] = useState(0)
 
   const userPerPage = 10;
@@ -13,7 +13,7 @@ const Table = ({ dataKeys, data, query }) => {
   const displayUsers = data.slice(pagesVisited, userPerPage + pagesVisited)
   // console.log(displayUsers)
   const totalPage = Math.ceil(data.length / userPerPage)
-  console.log(totalPage)
+  // console.log(totalPage)
 
 
   
@@ -23,52 +23,52 @@ const Table = ({ dataKeys, data, query }) => {
       <p>No Data Found</p>
     )
   }
-  else if (query != "") {
-    return (
-      <div>
-        <table className={styles.table}>    
-          <thead>
-          <tr>
-          {(dataKeys).map((data, index) => {
-              return <th key={index}>{data}</th>
-            })}
-          </tr>
-           </thead>
-           <tbody>
-            {(data).slice(start, count).map((item, id) => (
-              <tr className={styles.row} key={id}>
-                {(Object.keys(item)).map((data, index) => (
-                  <td key={index}>{item[data]}</td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        {
-          data.length > count && (
-            <div className={styles.btnContainer}>
-              <div>
-                <button className={styles.btn} 
-                onClick={() => {
-                  setStart(prev => prev - 10);
-                  setCount(prev => prev - 10);
+  // else if (query != "") {
+  //   return (
+  //     <div>
+  //       <table className={styles.table}>    
+  //         <thead>
+  //         <tr>
+  //         {(dataKeys).map((data, index) => {
+  //             return <th key={index}>{data}</th>
+  //           })}
+  //         </tr>
+  //          </thead>
+  //          <tbody>
+  //           {(data).slice(start, count).map((item, id) => (
+  //             <tr className={styles.row} key={id}>
+  //               {(Object.keys(item)).map((data, index) => (
+  //                 <td key={index}>{item[data]}</td>
+  //               ))}
+  //             </tr>
+  //           ))}
+  //         </tbody>
+  //       </table>
+  //       {
+  //         data.length > count && (
+  //           <div className={styles.btnContainer}>
+  //             <div>
+  //               <button className={styles.btn} 
+  //               onClick={() => {
+  //                 setStart(prev => prev - 10);
+  //                 setCount(prev => prev - 10);
                   
-                }}>prev</button>
-              </div>
-              <div>
-                <button className={styles.btn} onClick={() => {
-                  setStart(prev => prev + 10);
-                  setCount(prev => prev + 10);
+  //               }}>prev</button>
+  //             </div>
+  //             <div>
+  //               <button className={styles.btn} onClick={() => {
+  //                 setStart(prev => prev + 10);
+  //                 setCount(prev => prev + 10);
                   
-                }}>next</button>
-              </div>
-            </div>
-          )
-        }
-      </div>
-    )
+  //               }}>next</button>
+  //             </div>
+  //           </div>
+  //         )
+  //       }
+  //     </div>
+  //   )
 
-  }
+  // }
 
   else {
     return (
